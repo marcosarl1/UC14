44fdaf5b6519dcff5d98b5ec6f9b5a52bc26678a
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 17:22:11 -0300

    feat: Separation of responsibility

--- Atividade01/src/main/java/com/senac/controller/MovieController.java
+++ Atividade01/src/main/java/com/senac/controller/MovieController.java
@@ -1,27 +1,21 @@
 package com.senac.controller;
 
 import com.senac.model.Movie;
-import com.senac.model.Review;
 import com.senac.service.MovieService;
-import com.senac.service.ReviewService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
-    private final ReviewService reviewService;
 
-    public MovieController(MovieService movieService, ReviewService reviewService) {
+    public MovieController(MovieService movieService) {
         this.movieService = movieService;
-        this.reviewService = reviewService;
     }
 
     @GetMapping
@@ -43,14 +37,5 @@ public class MovieController {
         }
         movieService.addMovie(movie);
         return "redirect:/movies";
-    }
-
-    @GetMapping("/details/{id}")
-    public String movieDetails(@PathVariable Integer id, Model model) {
-        List<Review> reviews = reviewService.getMovieReviewById(id);
-        model.addAttribute("movie", movieService.getMovieById(id));
-        model.addAttribute("review", new Review());
-        model.addAttribute("reviews", reviews);
-        return "movie-details";
     }
 }
--- Atividade01/src/main/java/com/senac/controller/ReviewController.java
+++ Atividade01/src/main/java/com/senac/controller/ReviewController.java
@@ -1,22 +1,33 @@
 package com.senac.controller;
 
 import com.senac.model.Review;
+import com.senac.service.MovieService;
 import com.senac.service.ReviewService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/review")
+@RequestMapping("/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final MovieService movieService;
 
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, MovieService movieService) {
         this.reviewService = reviewService;
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/{id}")
+    public String getReviews(@PathVariable Integer id, Model model) {
+        List<Review> reviews = reviewService.getMovieReviewById(id);
+        model.addAttribute("movie", movieService.getMovieById(id));
+        model.addAttribute("review", new Review());
+        model.addAttribute("reviews", reviews);
+        return "reviews";
     }
 
     @PostMapping("/add/{movieId}")
