ef47c59756fa7280d3ee97105d4fa28007cfc0f9
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 20:00:28 -0300

    feat: ReviewRepository

---
+++ Atividade02/src/main/java/com/senac/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.senac.repository;
+
+import com.senac.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
