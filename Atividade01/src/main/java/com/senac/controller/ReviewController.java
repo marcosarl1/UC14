4a7d70c5ec050e625018814410a8534104171414
Marcos Júnior <marcosajr11@gmail.com>
2024-12-28 21:42:41 -0300

    feat: ReviewController class

---
+++ Atividade01/src/main/java/com/senac/controller/ReviewController.java
@@ -0,0 +1,27 @@
+package com.senac.controller;
+
+import com.senac.model.Review;
+import com.senac.service.ReviewService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/review")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/add/{movieId}")
+    public String addReview(@PathVariable Integer movieId, @ModelAttribute Review review, Model model) {
+        reviewService.addReview(movieId, review);
+        return "redirect:/movies/details";
+    }
+}
