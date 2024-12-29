2e1be726a0ded890c44d4ae96ca5c9259cb4f687
Marcos Júnior <marcosajr11@gmail.com>
2024-12-28 22:19:36 -0300

    fix: Reviews are showing properly

--- Atividade01/src/main/java/com/senac/controller/MovieController.java
+++ Atividade01/src/main/java/com/senac/controller/MovieController.java
@@ -3,18 +3,23 @@ package com.senac.controller;
 import com.senac.model.Movie;
 import com.senac.model.Review;
 import com.senac.service.MovieService;
+import com.senac.service.ReviewService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
+    private final ReviewService reviewService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, ReviewService reviewService) {
         this.movieService = movieService;
+        this.reviewService = reviewService;
     }
 
     @GetMapping
@@ -37,8 +42,10 @@ public class MovieController {
 
     @GetMapping("/details/{id}")
     public String movieDetails(@PathVariable Integer id, Model model) {
+        List<Review> reviews = reviewService.getMovieReviewById(id);
         model.addAttribute("movie", movieService.getMovieById(id));
         model.addAttribute("review", new Review());
+        model.addAttribute("reviews", reviews);
         return "movie-details";
     }
 }
