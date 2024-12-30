554fa0b952f9953cd32d30947f8a0d94e27f182d
Marcos Júnior <marcosajr11@gmail.com>
2024-12-30 18:12:16 -0300

    MINOR: Change on ID strategy

--- Atividade02/src/main/java/com/senac/model/Movie.java
+++ Atividade02/src/main/java/com/senac/model/Movie.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.NotNull;
 public class Movie {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotBlank(message = "Por favor, informe um título válido.")
--- Atividade02/src/main/java/com/senac/model/Review.java
+++ Atividade02/src/main/java/com/senac/model/Review.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.NotNull;
 public class Review {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
