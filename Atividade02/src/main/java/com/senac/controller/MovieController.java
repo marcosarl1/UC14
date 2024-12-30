62fdbf758611521be1a3901e4bcc28c2bb07eef8
Marcos Júnior <marcosajr11@gmail.com>
2024-12-30 18:08:37 -0300

    fix: Correct endpoint path

--- Atividade02/src/main/java/com/senac/controller/MovieController.java
+++ Atividade02/src/main/java/com/senac/controller/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/movies")
+@RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
 
@@ -29,19 +29,19 @@ public class MovieController {
         return ResponseEntity.ok(movie);
     }
 
-    @PostMapping()
+    @PostMapping(("/add"))
     public ResponseEntity<Movie> addMovie(@Valid @RequestBody Movie movie) {
         Movie savedMovie = movieService.addMovie(movie);
         return ResponseEntity.ok(savedMovie);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Integer id, @Valid @RequestBody Movie movie) {
         Movie updatedMovie = movieService.updateMovie(id, movie);
         return ResponseEntity.ok(updatedMovie);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Movie> deleteMovie(@PathVariable Integer id) {
         movieService.deleteMovie(id);
         return ResponseEntity.noContent().build();
