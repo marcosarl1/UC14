65df43e9d4420ce77c54fe73686e2959a3b8ecb6
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 22:00:06 -0300

    feat: ReviewService methods

--- Atividade02/src/main/java/com/senac/service/ReviewService.java
+++ Atividade02/src/main/java/com/senac/service/ReviewService.java
@@ -1,4 +1,13 @@
 package com.senac.service;
 
+import com.senac.model.Movie;
+
+import java.util.List;
+
 public interface ReviewService {
+    List<Movie> getReviews();
+    Movie getReviewById(Integer id);
+    Movie addReview(Movie movie);
+    Movie updateReview(Integer id, Movie movie);
+    void deleteReview(Integer id);
 }
