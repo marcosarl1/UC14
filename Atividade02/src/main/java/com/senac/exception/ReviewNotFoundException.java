18a0d473abdc526b804d2f86d95ae3750193e441
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 22:08:35 -0300

    feat: ReviewNotFoundException class

---
+++ Atividade02/src/main/java/com/senac/exception/ReviewNotFoundException.java
@@ -0,0 +1,7 @@
+package com.senac.exception;
+
+public class ReviewNotFoundException extends RuntimeException {
+    public ReviewNotFoundException(String message) {
+        super(message);
+    }
+}
