3dd02dbebc8c3e8eb2da576828443e32f88c487f
Marcos Júnior <marcosajr11@gmail.com>
2024-12-30 18:09:35 -0300

    MINOR: Added ResponseStatus annotation

--- Atividade02/src/main/java/com/senac/exception/ReviewNotFoundException.java
+++ Atividade02/src/main/java/com/senac/exception/ReviewNotFoundException.java
@@ -1,5 +1,9 @@
 package com.senac.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ReviewNotFoundException extends RuntimeException {
     public ReviewNotFoundException(String message) {
         super(message);
