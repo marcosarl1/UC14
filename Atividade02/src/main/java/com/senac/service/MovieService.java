c24b515ed4094b80ff051e1afee3f2c7c4ec23e6
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 21:45:31 -0300

    feat: MovieService interface

---
+++ Atividade02/src/main/java/com/senac/service/MovieService.java
@@ -0,0 +1,13 @@
+package com.senac.service;
+
+import com.senac.model.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+    List<Movie> getMovies();
+    Movie getMovieByid(Integer id);
+    Movie addMovie(Movie movie);
+    Movie updateMovie(Integer id, Movie movie);
+    void deleteMovie(Integer id);
+}
