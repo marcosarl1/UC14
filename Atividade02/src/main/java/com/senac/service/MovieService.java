2d53662c4052fe0b8e248f113f785c85985a8f5e
Marcos Júnior <marcosajr11@gmail.com>
2024-12-29 21:59:42 -0300

    refactor: camelCase method

--- Atividade02/src/main/java/com/senac/service/MovieService.java
+++ Atividade02/src/main/java/com/senac/service/MovieService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface MovieService {
     List<Movie> getMovies();
-    Movie getMovieByid(Integer id);
+    Movie getMovieById(Integer id);
     Movie addMovie(Movie movie);
     Movie updateMovie(Integer id, Movie movie);
     void deleteMovie(Integer id);
