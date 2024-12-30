c6e0b2200773444b61c29c56dd220b65e0922184
Marcos Júnior <marcosajr11@gmail.com>
2024-12-30 18:13:29 -0300

    feat: Movie validation on ReviewService

--- Atividade02/src/main/java/com/senac/service/impl/ReviewServiceImpl.java
+++ Atividade02/src/main/java/com/senac/service/impl/ReviewServiceImpl.java
@@ -1,18 +1,24 @@
 package com.senac.service.impl;
 
+import com.senac.exception.MovieNotFoundException;
 import com.senac.exception.ReviewNotFoundException;
 import com.senac.model.Review;
+import com.senac.repository.MovieRepository;
 import com.senac.repository.ReviewRepository;
 import com.senac.service.ReviewService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
+    private final MovieRepository movieRepository;
     private final String reviewNotFoundMsg = "Análise não encontrada com ID: ";
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, MovieRepository movieRepository) {
         this.reviewRepository = reviewRepository;
+        this.movieRepository = movieRepository;
     }
 
     @Override
@@ -28,12 +34,14 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public Review addReview(Review review) {
+        validateMovieExists(review.getMovie().getId());
         return reviewRepository.save(review);
     }
 
     @Override
     public Review updateReview(Integer id, Review review) {
         validateId(id);
+        validateMovieExists(review.getMovie().getId());
         return reviewRepository.findById(id)
                 .map(existingReview -> {
                     review.setId(id);
@@ -53,6 +61,12 @@ public class ReviewServiceImpl implements ReviewService {
     private void validateId(Integer id) {
         if (id == null || id <= 0) {
             throw new IllegalArgumentException("Id do filme deve ser um número positivo");
+        }
+    }
+
+    private void validateMovieExists(Integer movieId) {
+        if (movieId == null || !movieRepository.existsById(movieId)) {
+            throw new MovieNotFoundException("Filme não encontrado com ID: " + movieId);
         }
     }
 }
