8a42ab23c799bd47aba7910a87c8ceb6f138126a
Marcos Júnior <marcosajr11@gmail.com>
2025-01-02 13:00:59 -0300

    feat: update and delete

--- Atividade03/Atividade03Frontend/assets/js/reviews.js
+++ Atividade03/Atividade03Frontend/assets/js/reviews.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = 'http://localhost:8080/movies';
 let currentMovieId = null;
+let reviewToDelete = null;
 
 function getMovieIdFromUrl() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -71,6 +72,60 @@ async function handleReviewForm(event) {
     }
 }
 
+async function handleReviewUpdate(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    if (!form.checkValidity()) {
+        event.stopPropagation();
+        form.classList.add('was-validated');
+    }
+
+    const reviewId = document.getElementById('updateReviewId').value.trim();
+    const updateReviewData = {
+        review: document.getElementById('updateReview').value,
+        score: parseFloat(document.getElementById('updateScore').value)
+    }
+
+    const res = await fetch(`${API_BASE_URL}/${currentMovieId}/reviews/update/${reviewId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateReviewData)
+    });
+
+    if (!res.ok) {
+        throw new Error('Erro ao atualizar análise');
+    }
+
+    const modal = bootstrap.Modal.getInstance(document.getElementById('updateReviewModal'));
+    modal.hide();
+    form.reset();
+    form.classList.remove('was-validated');
+    await fetchReviews();
+}
+
+async function deleteReview(){
+    if (!reviewToDelete) return;
+
+    try {
+        const res = await fetch(`${API_BASE_URL}/${currentMovieId}/reviews/delete/${reviewToDelete}`, {
+            method: 'DELETE'
+        });
+
+        if (!res.ok) throw new Error('Erro ao deletar análise.')
+
+        const modal = bootstrap.Modal.getInstance(document.getElementById('deleteReviewModal'));
+        modal.hide();
+        await fetchReviews();
+    } catch (error) {
+        console.error('Erro ao deletar análise: ', error);
+    } finally {
+        reviewToDelete = null;
+    }
+}
+
 function renderMovie(movie) {
     document.getElementById('movieTitle').textContent = movie.title;
     document.getElementById('movieSynopsis').textContent = movie.synopsis;
@@ -101,13 +156,13 @@ function renderReviews(reviews) {
                             <i class="bi bi-three-dots-vertical"></i></button>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
-                                <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#updateReviewModal"><i
-                                        class="bi bi-pencil me-2"></i>Editar</a>
+                                <a class="dropdown-item" data-bs-target="#updateReviewModal" data-bs-toggle="modal" onclick="openUpdateReviewModal(${review.id}, '${review.review}', ${review.score})"><i
+                                        class="bi bi-pencil me-2" ></i>Editar</a>
                             </li>
                             <li><hr class="dropdown-divider"></li>
                             <li>
                                 <a class="dropdown-item text-danger" data-bs-toggle='modal'
-                                   data-bs-target="#deleteReviewModal"><i
+                                   data-bs-target="#deleteReviewModal" onclick="openDeleteModal(${review.id})"><i
                                         class="bi bi-trash me-2"></i>Excluir</a>
                             </li>
                         </ul>
@@ -135,11 +190,31 @@ function generateStars(score) {
     return starsHtml;
 }
 
+function openUpdateReviewModal(id, review, score) {
+    document.getElementById('updateReviewId').value = id;
+    document.getElementById('updateReview').value = review;
+    document.getElementById('updateScore').value = score;
+}
+
+function openDeleteModal(reviewId) {
+    reviewToDelete = reviewId;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     fetchMovie();
 
     const reviewForm = document.getElementById('reviewForm');
     if (reviewForm) {
         reviewForm.addEventListener('submit', handleReviewForm);
+    }
+
+    const updateReviewForm = document.getElementById('updateReviewForm');
+    if (updateReviewForm) {
+        updateReviewForm.addEventListener('submit', handleReviewUpdate);
+    }
+
+    const confirmDeleteButton = document.getElementById('confirmDeleteButton');
+    if (confirmDeleteButton) {
+        confirmDeleteButton.addEventListener('click', deleteReview);
     }
 })
